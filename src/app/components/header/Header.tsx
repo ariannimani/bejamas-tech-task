3d21af3f49bde2c82a800c17e90e0df83c19fa5d
aarriiaan <aarriiaann@gmail.com>
2023-05-18 23:40:35 +0200

    fixed typescript error for CardWrapper

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -7,6 +7,7 @@ const Header = () => {
   return (
     <div className="flex justify-between w-full p-6 border-b-2">
       <Image src={LogoIcon} alt="logo" />
+      {/* @ts-expect-error Server Component */}
       <CartWrapper />
     </div>
   );
